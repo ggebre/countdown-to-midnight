8c7f2e645b84502221f1401810ef4c6a565b3d8a
Getu Gebre <getu.gebre.gg@gmail.com>
2020-09-29 12:58:06 -0500

    done

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,18 @@
 #write your code here
 
-def countdown
+def countdown (num)
+    until num == 0 do 
+        puts "#{num} SECOND(S)!"
+        num -= 1
+    end
+    "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep (num)
+    until num == 0 do 
+        puts "#{num} SECOND(S)!"
+        sleep(1)
+        num -= 1
+    end
+    "HAPPY NEW YEAR!"
 end
